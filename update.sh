5cd07e26e80b33d7367c5545bdb7a3e0aae94e39
CodeVNew <171561729+codevnew@users.noreply.github.com>
2026-07-01 08:15:00 +0000

    commit

---
+++ update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+change=0
+username=scratch-extension-demos                                  #"$(gh api user --jq .login)"
+filepath1=$(pwd)
+filepath2="/workspaces/scratch-extension-demos/"
+
+echo "$filepath1 $username"
+
+if [ "$filepath1" != $filepath2 ]; then
+	change=1
+	cd $filepath2
+fi
+
+git fetch upstream
+echo
+git merge upstream/master -m "sync upstream"
+echo
+git push origin master
+echo
+git fetch
+echo
+git add /workspaces/$username/.
+echo
+git commit -m "commit"
+echo
+git pull
+echo
+git push
+echo
+git merge -m "merge"
+
+if [ "$change" -eq 1 ]; then
+	cd $filepath1
+fi
